2e276f014c4c42f48beb059288809faa50348e1d
lonk <lonk@live.fr>
2015-04-13 23:34:17 +0200

    Improves the pertelian's script to handle properly errors

--- scripts/pertelian.py
+++ scripts/pertelian.py
@@ -3,9 +3,12 @@ import socketserver
 import urllib
 import telnetlib
 import math
+import time
+from threading import Thread
 
 PORT = 8006
 LENGTH = 20
+tn = None
 
 def connect():
     return telnetlib.Telnet("127.0.0.1", 13666)
@@ -29,27 +32,31 @@ def configPertelian(tn):
     send(tn, "widget_add perte line3 string")
     send(tn, "widget_add perte line4 string")
 
+class telnetLogin(Thread):
+    global tn
+    def run(self):
+        global tn
+        while True:
+            if tn == None:
+                try:
+                    tn = connect()
+                    configPertelian(tn)
+                    writeLine(tn, 2, 'WARNING')
+                    writeLine(tn, 3, 'BuckUTT is coming !')
+                except ConnectionRefusedError:
+                    print("Connexion au Pertelian échouée. Nouvel essai dans 60 secondes...")
+            time.sleep(1)
+
 class MyHandler(http.server.SimpleHTTPRequestHandler):
+    global tn, threadTelnet
     def do_GET(self):
+        global tn
+        print(tn)
         # Parse query data & params to find out what was passed
         parsedParams = urllib.parse.urlparse(self.path)
         print(parsedParams)
 
         queryParsed = urllib.parse.parse_qs(parsedParams.query)
-
-        if 'l1' not in queryParsed:
-            queryParsed['l1'] = ['']
-        if 'l2' not in queryParsed:
-            queryParsed['l2'] = ['']
-        if 'l3' not in queryParsed:
-            queryParsed['l3'] = ['']
-        if 'l4' not in queryParsed:
-            queryParsed['l4'] = ['']
-
-        writeLine(tn, 1, queryParsed['l1'][0])
-        writeLine(tn, 2, queryParsed['l2'][0])
-        writeLine(tn, 3, queryParsed['l3'][0])
-        writeLine(tn, 4, queryParsed['l4'][0])
 
         if parsedParams.path == "/device.html":
             hostname = open("/etc/hostname", "r")
@@ -60,17 +67,39 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
             self.send_header("Content-length", len(str(deviceId)))
             self.end_headers()
             self.wfile.write(str(deviceId).encode('ascii'))
-        else:
-            http.server.SimpleHTTPRequestHandler.do_GET(self);
+        elif parsedParams.path == "/update.html":
+            try:
+                if 'l1' not in queryParsed:
+                    queryParsed['l1'] = ['']
+                if 'l2' not in queryParsed:
+                    queryParsed['l2'] = ['']
+                if 'l3' not in queryParsed:
+                    queryParsed['l3'] = ['']
+                if 'l4' not in queryParsed:
+                    queryParsed['l4'] = ['']
 
-tn = connect()
-configPertelian(tn)
-writeLine(tn, 2, 'WARNING')
-writeLine(tn, 3, 'BuckUTT is coming !')
+                writeLine(tn, 1, queryParsed['l1'][0])
+                writeLine(tn, 2, queryParsed['l2'][0])
+                writeLine(tn, 3, queryParsed['l3'][0])
+                writeLine(tn, 4, queryParsed['l4'][0])
+            except:
+                tn = None
+                print("Connexion au Pertelian perdue. Reconnexion en cours...")
+
+            http.server.SimpleHTTPRequestHandler.do_GET(self)
+        else:
+            http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 Handler = MyHandler
 
 httpd = socketserver.TCPServer(("127.0.0.1", PORT), Handler)
 
-print("Le serveur est lance sur le port", PORT)
-httpd.serve_forever()
+threadTelnet = telnetLogin()
+threadTelnet.start()
+
+try:
+    print("Le serveur est lance sur le port", PORT)
+    httpd.serve_forever()
+except:
+    httpd.shutdown()
+    httpd.server_close()
